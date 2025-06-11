33143852b72d81951c6a29eedd74322b7602a18a
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2025-06-11 14:37:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "GenieBotV2",
+  platform: "arduino-pico",
+  title: "GenieBotV2",
+  description: "ชุดหุ่นยนต์ อปท. ประถม",
+  author: "PrinceBot",
+  website: "https://PrinceBotShop.com",
+  email: "print081@gmail.com",
+  git: "https://github.com/PrinceBot-Ratthanin/GenieBotV2",
+  image: "/static/robot.png",
+  version: "1.0.0",
+};
