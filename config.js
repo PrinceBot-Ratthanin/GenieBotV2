f916ab7b1e69f9cafc5a8f84e12522be766565eb
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2025-06-18 10:38:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
   email: "print081@gmail.com",
   git: "https://github.com/PrinceBot-Ratthanin/GenieBotV2",
   image: "/static/robot.png",
-  version: "1.0.0",
+  version: "1.2.0",
 };
